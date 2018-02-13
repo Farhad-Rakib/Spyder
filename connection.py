92598b239443cdb6846e0aecfaacfb9f699901ba
Farhad <farhad.rakib@datasoft-bd.com>
2018-02-13 09:55:37 +0600

    Database connection file changed,exception handling introduce

--- connection.py
+++ connection.py
@@ -1,6 +1,15 @@
 import mysql.connector
-import _mysql     
+import _mysql
+from mysql.connector import errorcode
 
-connection = mysql.connector.connect(user='root', password='',
+try:
+    connection = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
                               database='spyder')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
